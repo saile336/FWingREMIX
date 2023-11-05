9eb7c2146ce52e47ca2b45b36faeb73ca2ba61c8
Saile336 <elias.nybondas@gmail.com>
2023-11-05 17:07:37 +0200

    server updated for kide fetch

--- server/server.js
+++ server/server.js
@@ -27,4 +27,58 @@ app.post('/api/addData', async (req, res) => {
   res.send('Data added successfully');
 });
 
+
+//Kide fetch
+let orgs = [
+  {
+    name: "TLK",
+    kideUrl: "https://api.kide.app/api/companies/8216a1bc-760d-407b-9c77-5e26a041a25c",
+    data: {}
+  },
+  {
+    name: "HanSe SF",
+    kideUrl: "https://api.kide.app/api/companies/90d58532-87be-4a30-b4e3-6053db20caa5",
+    data: {}
+  },
+  {
+    name: "Commedia rf",
+    kideUrl: "https://api.kide.app/api/companies/b7b04c01-6c49-4c74-81da-9f4147aca6db",
+    data: {}
+  },
+  {
+    name: "Kult rf",
+    kideUrl: "https://api.kide.app/api/companies/d12f83ed-8efa-40d9-8288-e62f1ac8fc43",
+    data: {}
+  }
+]
+
+const fetchKide = async () => {
+  console.log("fetchKide()");
+
+
+  try {
+    orgs.forEach(org => {
+      fetch(org.kideUrl)
+        .then(res => res.json())
+        .then(data => {
+          org.data = data;
+        })
+    })
+
+    const data = await res.json();
+    this.kideData = data;
+  }
+
+  catch (error) {
+    console.log("Could not fetch kide," + error);
+  }
+};
+
+setInterval(fetchKide, 1000*60*60);
+
+app.get('/api/getKide', async (res) => {
+  const data = orgs;
+  res.json(data);
+});
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
