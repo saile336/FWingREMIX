cdb7ca8a2055e22d4b6102bcf942c842a3e8b7d9
Robin Daniel Tildeman <Robin@tildeman.ax>
2023-11-17 00:27:01 +0200

    Widgets and Diet settings

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,50 +1,110 @@
 <template>
     <div class="schedule-button-container">
-      <button class="schedule-button" @click="showInputField">
-        <i class="icon">A</i> Add to Schedule
-      </button>
-      <input
-        v-if="inputVisible"
-        type="text"
-        v-model="link"
-        class="link-input"
-        placeholder="Enter link here"
-        @keyup.enter="saveLink"
-      />
+        <button class="action-button" @click="toggleInputField">
+            Add to Schedule
+        </button>
+
+
+        <input v-if="inputVisible" type="text" v-model="link" class="link-input" placeholder="Enter link here"
+            @keyup.enter="saveLink" />
+
+        <button class="action-button" @click="toggleDietOptions">
+            Diet
+        </button>
+
+        <div v-if="showDietOptions" class="dietOptions">
+            <button v-for="(selected, diet) in diets" :key="diet" :class="{ active: selected }" @click="selectDiet(diet)">
+                {{ diet }}
+            </button>
+        </div>
+
+        <button class="action-button" @click="toggleWidgetOptions">
+            Widgets
+        </button>
+
+        <p v-if="showWidgetOptions" class="disclaimer">Choose up to 2 widgets:</p>
+
+        <div v-if="showWidgetOptions" class="widgetOptions">
+            <button v-for="(selected, widget) in widgets" :key="widget" :class="{ active: selected }"
+                @click="selectWidget(widget)">
+                {{ widget }}
+            </button>
+        </div>
     </div>
-  </template>
+</template>
   
-  <script>
-  export default {
+<script>
+export default {
     name: 'AddScheduleButton',
     data() {
-      return {
-        inputVisible: false,
-        link: '',
-      };
+        return {
+            inputVisible: false, // Check if we should show the link input
+            link: '', // The link to be saved
+            showDietOptions: false, // Controls visibility of diet options
+            diets: { Milk: false, Laktos: false, Vege: false, Gluten: false },
+            showWidgetOptions: false, // Controls visibility of widget options
+            widgets: { Schedule: false, Menu: false, Events: false, Weather: false }
+        };
     },
     methods: {
-      showInputField() {
-        this.inputVisible = true;
-      },
-      saveLink() {
-        localStorage.setItem('scheduleLink', this.link);
-        this.inputVisible = false;
-        this.link = ''; // Reset link input
-      }
+        toggleInputField() {
+            this.inputVisible = !this.inputVisible;
+        },
+        saveLink() {
+            localStorage.setItem('scheduleLink', this.link);
+            this.inputVisible = false;
+            this.link = ''; // Clearing input after saving
+        },
+        toggleDietOptions() {
+            this.showDietOptions = !this.showDietOptions;
+        },
+        selectDiet(option) {
+            this.diets[option] = !this.diets[option];
+            localStorage.setItem('Diets', JSON.stringify(this.diets));
+        },
+        toggleWidgetOptions() {
+            this.showWidgetOptions = !this.showWidgetOptions;
+        },
+        selectWidget(widget) {
+            const selectedWidgets = this.countSelectedWidgets();
+            if (!this.widgets[widget] && selectedWidgets >= 2) {
+                return; // Do nothing if 2 widgets are already selected
+            }
+            this.widgets[widget] = !this.widgets[widget];
+            localStorage.setItem('Widgets', JSON.stringify(this.widgets));
+        },
+        countSelectedWidgets() {
+            return Object.values(this.widgets).filter(val => val).length;
+        },
+        loadSettings() {
+            // Load saved settings for diets and widgets
+            const savedDiets = localStorage.getItem('Diets');
+            if (savedDiets) {
+                this.diets = JSON.parse(savedDiets);
+            }
+            const savedWidgets = localStorage.getItem('Widgets');
+            if (savedWidgets) {
+                this.widgets = JSON.parse(savedWidgets);
+            }
+        }
+    },
+    mounted() {
+        // Load saved settings when the component mounts
+        this.loadSettings();
     }
-  }
-  </script>
+}
+</script>
   
-  <style scoped>
-  .schedule-button-container {
+<style scoped>
+.schedule-button-container {
     display: flex;
     flex-direction: column;
     align-items: center;
     margin: 20px;
-  }
-  
-  .schedule-button {
+    gap: 10px;
+}
+
+.action-button {
     background-color: #4054B2;
     color: white;
     border: none;
@@ -57,31 +117,71 @@
     align-items: center;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
     transition: background-color 0.3s;
-    margin-bottom: 10px; /* Spacing between button and input field */
-  }
-  
-  .schedule-button i.icon {
-    background-color: white;
-    color: #4054B2;
-    border-radius: 50%;
-    margin-right: 10px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 25px;
-    height: 25px;
-  }
-  
-  .schedule-button:hover {
+}
+
+.action-button:hover {
     background-color: #2D3A8C;
-  }
-  
-  .link-input {
+}
+
+.link-input {
     border: 1px solid #ccc;
     border-radius: 4px;
     padding: 10px;
-    width: 90%; /* Adjust width as necessary */
+    width: 90%;
     margin-top: 10px;
-  }
-  </style>
+}
+
+.dietOptions button {
+    margin: 5px;
+    padding: 10px 15px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+.dietOptions button.active {
+    background-color: #4CAF50;
+    color: white;
+}
+
+.dietOptions {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 10px;
+}
+
+.widgetOptions button {
+    margin: 5px;
+    padding: 10px 15px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+.widgetOptions button.active {
+    background-color: #4CAF50;
+    color: white;
+}
+
+.widgetOptions {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 10px;
+}
+
+.widgetOptions .disclaimer {
+    margin-bottom: 10px;
+    font-size: 0.9em;
+    color: #666;
+}
+
+.disclaimer {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    font-size: 0.9em;
+    color: white;
+}
+</style>
   
