1cbf7226abb35a681e7d1f8a96e6d43267d3698a
Saile336 <elias.nybondas@gmail.com>
2023-12-03 21:16:45 +0200

    kide lite style

--- src/components/FetchKide.vue
+++ src/components/FetchKide.vue
@@ -49,18 +49,15 @@ export default {
 
 <template>
     <div id="events" v-if="isDataFetched">
-        <div v-for="forening in kideData" :key="forening">
+        <div class='föreningar' v-for="forening in kideData" :key="forening">
             <div class="event" v-for="event in forening.kideData.model.events" :key="event.id"
                 @click="clickHandler(event.id)">
                 <div class="image-container">
                     <img class="img" :src="imgUrl + event.mediaFilename" />
-                 <!--   <div class="QR">
-                        <VueQr :value="qr + event.id" />
-                    </div> -->
-                    <div :style="[event.salesStarted === true ? event.availability === 0 ? { 'backgroundColor': 'rgba(153, 0, 0,.7)' } : { 'backgroundColor': 'rgba(51, 153, 51,.7)' } : {}]"
+                    <div :style="[event.salesStarted === true ? event.availability === 0 ? { 'color': '#800000' } : { 'color': '#006600' } : {}]"
                         class="details">
                         <div class="name">{{ event.name }}</div>
-                        <div class="place">{{ event.place }}</div>
+                        <div class="date">{{ new Date(event.dateActualFrom).toLocaleDateString('fi-FI') }}</div>
                     </div>
                 </div>
             </div>
@@ -69,35 +66,41 @@ export default {
 </template>
 
 <style scoped>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
 h2 {
     font-size: 40px;
     margin: 0;
     text-align: center;
 }
 
-
 #events {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -55%);
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    width: 100%;
-    height: 75vh;
+    border-radius: 15px;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    height: 80vh;
+    width: 85vw;
+    background-color: white;
 }
 
 .event {
-    margin-top: 25px;
-    font-size: 25px;
     border-radius: 10px;
     overflow: hidden;
-    box-shadow: 15px 15px 1px 1px rgba(0, 0, 0, 0.5);
+    height: 150px;
+    margin: 30px 25px 0 25px;
 }
 
 .event:hover {
-    margin-top: 25px;
-    font-size: 25px;
-    border-radius: 10px;
-    overflow: hidden;
-    box-shadow: 15px 15px 1px 1px rgba(0, 0, 0, 0.5);
     cursor: pointer;
     scale: 1.1;
     transition: .1s;
@@ -118,26 +121,30 @@ h2 {
 
 
 .details {
-    display: block;
-    background-color: rgba(0, 0, 0, .7);
-    padding: 10px;
+    font-weight: 500;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap:0;
+    line-height: 1;
+    background-color: rgba(255, 255, 255, 0.5);
     position: absolute;
     bottom: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    width: 75%;
+    width: 100%;
+    height: 100%;
     border-radius: 10px 10px 0 0;
 }
 
 .name {
     text-align: center;
-    font-size: 20px;
+    font-size: 40px;
     z-index: 12;
 }
 
-.place {
+.date {
     text-align: center;
-    font-size: 15px;
+    font-size: 30px;
 
 }
 
